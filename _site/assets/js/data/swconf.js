8d819734f5609c3aadd9f991c9117bb9e3cb305d
skykhs3 <skykhs3@naver.com>
2024-06-16 04:58:48 +0900

    feat: .gitignore

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1718480880',resources: [
+    cacheName: 'chirpy-1718481318',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
