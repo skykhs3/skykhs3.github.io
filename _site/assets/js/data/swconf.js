895803f874e873b9f3f1a5f275e6ce7502e53e08
skykhs3 <skykhs3@naver.com>
2024-06-16 04:48:42 +0900

    init: page

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1718480581',resources: [
+    cacheName: 'chirpy-1718480880',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
