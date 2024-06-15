d0bc6dd1e8c840690181dada1475f7627c98153a
skykhs3 <skykhs3@naver.com>
2024-06-16 04:27:06 +0900

    chirpy init

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,24 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1718479566',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
