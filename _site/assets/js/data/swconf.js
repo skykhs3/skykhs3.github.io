7814fea4a9b8f84ee75be8b455f53ab27c6914de
skykhs3 <skykhs3@naver.com>
2024-09-11 18:20:33 +0900

    init submodule

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1726045483',resources: [
+    cacheName: 'chirpy-1726046402',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
