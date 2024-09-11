46a05bff9b16da57e0e95517dc016580c3248940
skykhs3 <skykhs3@naver.com>
2024-09-11 18:07:19 +0900

    bundle exec

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1726041973',resources: [
+    cacheName: 'chirpy-1726045483',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
